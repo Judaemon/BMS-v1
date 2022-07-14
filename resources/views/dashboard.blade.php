8bea399380ed2a6d977f453e603bbfd00e682aaa
John Ray Judaya <judaya93@gmail.com>
2022-07-14 12:45:14 +0800

    LaraStarter Kit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,105 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Dashboard') }}
+    </x-slot>
+
+    <div class="p-4 bg-white rounded-lg shadow-xs flex flex-col">
+        <div class="flex flex-col space-y-2 xl:space-x-2 xl:space-y-0 xl:flex-row">
+            <div class="p-2 bg-gray-800 flex-1 text-center">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">0</h5>
+                <h1 class="mb-2 text-1xl text-white">Registered Population</h1>
+            </div>
+            <div class="p-2 bg-gray-800 flex-1 text-center">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">0</h5>
+                <h1 class="mb-2 text-1xl text-white">List of Request</h1>
+            </div>
+            <div class="p-2 bg-gray-800 flex-1 text-center">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">0</h5>
+                <h1 class="mb-2 text-1xl text-white">Registered Voter</h1>
+            </div>
+            <div class="p-2 bg-gray-800 flex-1 text-center">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">0</h5>
+                <h1 class="mb-2 text-1xl text-white">Male</h1>
+            </div>
+            <div class="p-2 bg-gray-800 flex-1 text-center">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-white ">0</h5>
+                <h1 class="mb-2 text-1xl text-white">Female</h1>
+            </div>
+        </div>
+
+        <div class="flex flex-row">
+            <div class="p-2 basis-3/5">
+                <h2 class="my-6 text-1xl font-semibold text-gray-700 text-center">
+                    Current Barangay Member
+                </h2>
+                <div class="max-w-2xl mx-auto">
+                    <div class="flex flex-col">
+                    <div class="overflow-x-auto shadow-md sm:rounded-lg">
+                        <div class="inline-block min-w-full align-middle">
+                            <div class="overflow-hidden ">
+                                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                                    <thead class="bg-gray-100 dark:bg-gray-700">
+                                        <tr>
+                                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400 w-2/5">
+                                                Name
+                                            </th>
+                                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                                Position
+                                            </th>
+                                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                                Length of employment
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Names</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Position</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Year</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="p-2 basis-2/5">
+                <h2 class="my-6 text-1xl font-semibold text-gray-700 text-center">
+                    Puroks/Areas
+                </h2>
+                <div class="max-w-2xl mx-auto">
+                    <div class="flex flex-col">
+                    <div class="overflow-x-auto shadow-md sm:rounded-lg">
+                        <div class="inline-block min-w-full align-middle">
+                            <div class="overflow-hidden ">
+                                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-700">
+                                    <thead class="bg-gray-100 dark:bg-gray-700">
+                                        <tr>
+                                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                                Puroks/Areas
+                                            </th>
+                                            <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                                                Population
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-500 whitespace-nowrap dark:text-white">Position</td>
+                                            <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">Year</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</x-app-layout>
---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,105 @@
+<aside class="z-20 hidden w-64 overflow-y-auto bg-white md:block flex-shrink-0">
+    <div class="py-4 text-gray-500">
+        <a class="ml-6 text-lg font-bold text-gray-800" href="{{ route('dashboard') }}">
+            Barangay System
+        </a>
+
+        <ul class="mt-6">
+            <li class="relative px-6 py-3">
+                <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                    <x-slot name="icon">
+                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                            <path d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
+                        </svg>
+                    </x-slot>
+                    {{ __('Dashboard') }}
+                </x-nav-link>
+            </li>
+
+            <li class="relative px-6 py-3">
+                <x-nav-link href="{{ route('users.index') }}" :active="request()->routeIs('users.index')">
+                    <x-slot name="icon">
+                        <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
+                        </svg>
+                    </x-slot>
+                    {{ __('Residents Information') }}
+                </x-nav-link>
+            </li>
+
+            <li class="relative px-6 py-3">
+                <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
+                    <x-slot name="icon">
+                        <svg class="h-5 w-5" aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" xmlns="http://www.w3.org/2000/svg" >
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
+                          </svg>
+                    </x-slot>
+                    {{ __('Blotter Records') }}
+                </x-nav-link>
+            </li>
+
+            <li class="relative px-6 py-3">
+                <x-nav-link href="{{ route('about') }}" :active="request()->routeIs('about')">
+                    <x-slot name="icon">
+                        <svg class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                          </svg>
+                    </x-slot>
+                    {{ __('Settlement Schedules') }}
+                </x-nav-link>
+            </li>
+
+            <li class="relative px-6 py-3">
+                <button class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800"
+                        @click="toggleMultiLevelMenu" aria-haspopup="true">
+                <span class="inline-flex items-center">
+                    <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
+                    </svg>
+                    <span class="ml-4">Setting</span>
+                </span>
+                    <svg class="w-4 h-4" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
+                        <path fill-rule="evenodd"
+                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                              clip-rule="evenodd"></path>
+                    </svg>
+                </button>
+                <template x-if="isMultiLevelMenuOpen">
+                    <ul x-transition:enter="transition-all ease-in-out duration-300"
+                        x-transition:enter-start="opacity-25 max-h-0" x-transition:enter-end="opacity-100 max-h-xl"
+                        x-transition:leave="transition-all ease-in-out duration-300"
+                        x-transition:leave-start="opacity-100 max-h-xl" x-transition:leave-end="opacity-0 max-h-0"
+                        class="p-2 mt-2 space-y-2 overflow-hidden text-sm font-medium text-gray-500 rounded-md shadow-inner bg-gray-50 dark:text-gray-400 dark:bg-gray-900"
+                        aria-label="submenu">
+
+                        <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+                            <x-nav-link href="{{ route('about') }}" class="!text-white" :active="request()->routeIs('about')">
+                                <x-slot name="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V8a2 2 0 00-2-2h-5m-4 0V5a2 2 0 114 0v1m-4 0a2 2 0 104 0m-5 8a2 2 0 100-4 2 2 0 000 4zm0 0c1.306 0 2.417.835 2.83 2M9 14a3.001 3.001 0 00-2.83 2M15 11h3m-3 4h2" />
+                                      </svg>
+                                </x-slot>
+                                {{ __('Accounts') }}
+                            </x-nav-link>
+                        </li>
+
+                        <li class="px-2 py-1 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200">
+                            <x-nav-link href="{{ route('about') }}" class="!text-white" :active="request()->routeIs('about')">
+                                <x-slot name="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                      </svg>
+                                </x-slot>
+                                {{ __('System Setting') }}
+                            </x-nav-link>
+                        </li>
+                    </ul>
+                </template>
+            </li>
+        </ul>
+    </div>
+</aside>
