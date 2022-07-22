f3119ac224c3ab0ea05e0742873ef3ee741ffe81
John Ray Judaya <judaya93@gmail.com>
2022-07-22 16:08:28 +0800

    fix selected option (pass selected value)

---
+++ resources/views/components/select-option.blade.php
@@ -0,0 +1,8 @@
+@props(['disabled' => false, 'selectedItem' => '', 'key', 'value'])
+
+<option {{ $disabled ? 'disabled' : '' }}  
+        @selected($selectedItem == $key)
+        {{ $attributes->merge(['class' => 'p-2 block text-sm text-gray-700']) }}
+        value="{{ $key}}">
+        {{ $value}}
+</option>
---
+++ resources/views/components/select.blade.php
@@ -0,0 +1,10 @@
+@props(['disabled' => false, 'options' => [], 'selectedItem' => ''])
+
+<select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus-within:text-primary-600']) !!}>
+    <x-select-option disabled selected key='null' value='Select option'></x-select-option>
+
+    @foreach($options as $key => $value)
+        <x-select-option selectedItem='{{ $selectedItem }}' key='{{ $key }}' value='{{ $value }}'></x-select-option>
+    @endforeach
+</select>
+
