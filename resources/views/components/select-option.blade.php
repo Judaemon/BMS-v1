f310510e18def055099b4b19e52a96d1d291b73a
John Ray Judaya <judaya93@gmail.com>
2022-07-25 12:17:37 +0800

    fix selected and isabled property

--- resources/views/components/select-option.blade.php
+++ resources/views/components/select-option.blade.php
@@ -1,6 +1,6 @@
 @props(['disabled' => false, 'selectedItem' => '', 'key', 'value'])
 
-<option {{ $disabled ? 'disabled' : '' }}  
+<option {{ $disabled ? 'disabled' : '' }}
         @selected($selectedItem == $key)
         {{ $attributes->merge(['class' => 'p-2 block text-sm text-gray-700']) }}
         value="{{ $key}}">
--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,12 @@
 @props(['disabled' => false, 'options' => [], 'selectedItem' => ''])
 
+@php
+    $disabledOption = $selectedItem == null ? false : true;
+    //  @disabled($selectedItem == null ? true : false)
+@endphp
+
 <select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'mt-1 border-gray-300 rounded-md shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 focus-within:text-primary-600']) !!}>
-    <x-select-option disabled selected key='null' value='Select option'></x-select-option>
+        <x-select-option disabled='{{ $disabledOption }}' key='null' value='Select option'></x-select-option>
 
     @foreach($options as $key => $value)
         <x-select-option selectedItem='{{ $selectedItem }}' key='{{ $key }}' value='{{ $value }}'></x-select-option>
