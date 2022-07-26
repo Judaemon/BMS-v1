c819c6bd0b6505dd0434c97127bf6d56cf04d005
John Ray Judaya <judaya93@gmail.com>
2022-07-26 16:42:00 +0800

    remove space

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,8 +14,6 @@ class DashboardController extends Controller
      */
     public function __invoke(Request $request)
     {
-        
-
         return view('dashboard', compact('users'));
     }
 }
