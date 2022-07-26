9a948b068c2dafb578271425739b79f6c2c640a0
John Ray Judaya <judaya93@gmail.com>
2022-07-26 16:42:47 +0800

    removed requests and fix view path

--- app/Http/Controllers/ResidentController.php
+++ app/Http/Controllers/ResidentController.php
@@ -3,13 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Resident;
-use App\Http\Requests\StoreResidentRequest;
-use App\Http\Requests\UpdateResidentRequest;
 
 class ResidentController extends Controller
 {
     public function index()
     {
-        return view('Residents.index');
+        return view('resident.index');
     }
 }
