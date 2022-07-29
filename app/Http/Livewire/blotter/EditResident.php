b6e3ba9f6cbf08710026786103a5731f9efe27f0
John Ray Judaya <judaya93@gmail.com>
2022-07-29 19:08:26 +0800

    readme update

---
+++ app/Http/Livewire/blotter/EditResident.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Blotter;
+
+use Livewire\Component;
+
+class EditResident extends Component
+{
+    public function render()
+    {
+        return view('livewire.blotter.edit-resident');
+    }
+}
